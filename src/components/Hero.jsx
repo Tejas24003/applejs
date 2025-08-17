ebb731835118468e5bac661143da6ccae46ba596
Tejas srivastava <tejassrivast@gmail.com>
2025-08-17 13:26:32 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import {heroVideo,smallHeroVideo} from '../utils';
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+ const handVideoSoruce = () => {
+  
+  if(window.innerWidth < 760){
+    setVideoSrc(smallHeroVideo)
+  }
+  else{
+    setVideoSrc(heroVideo)}
+
+ }
+
+ useEffect(()=>{
+  window.addEventListener('resize',handVideoSoruce);
+  return () => {
+    window.removeEventListener('resize',
+      handVideoSoruce)}
+    
+ },[])
+ 
+ 
   useGSAP(() => {
     gsap.to("#hero", {
       opacity: 1,
+      delay:1.5,
       duration: 2,
     });
   }, [])
@@ -14,6 +38,14 @@ const Hero = () => {
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">iPhone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12">
+        <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc} >
+          <source src={videoSrc} type="video/mp4" />
+        </video>
+        </div>
+      </div>
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+
       </div>
     </section>
   );
