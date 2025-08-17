9a706da4e4d4f9c5e5c55dd357eb7e1669c042a1
Tejas srivastava <tejassrivast@gmail.com>
2025-08-18 00:27:12 +0530

    added highlights section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,15 +29,20 @@ const Hero = () => {
   useGSAP(() => {
     gsap.to("#hero", {
       opacity: 1,
-      delay:1.5,
+      delay:2,
       duration: 2,
     });
+    gsap.to('#cta',{
+      opacity:1,
+      y:-50,
+      delay:2
+    })
   }, [])
 
   return (
     <section className="w-full nav-height bg-black relative">
-      <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero" className="hero-title">iPhone 15 Pro</p>
+      <div className="h-5/6 w-full flex justify-center items-center flex-col">
+        <p id="hero" className="hero-title ">iPhone 15 Pro</p>
         <div className="md:w-10/12 w-9/12">
         <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc} >
           <source src={videoSrc} type="video/mp4" />
@@ -45,7 +50,8 @@ const Hero = () => {
         </div>
       </div>
       <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
-
+     <a href="#highlight" className=" btn  ">Buy</a>
+     <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
